ff0ce1250af9c77a269388a8b1fcfbaa9d6251fe
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-04-27 11:50:43 +0530

    Create Anagrams.java
    Char comparison

---
+++ Anagrams.java
@@ -0,0 +1,139 @@
+/*
+Two strings, a and b
+
+, are called anagrams if they contain all the same characters in the same frequencies. For this challenge, the test is not case-sensitive. For example, the anagrams of CAT are CAT, ACT, tac, TCA, aTC, and CtA.
+
+Function Description
+
+Complete the isAnagram function in the editor.
+
+isAnagram has the following parameters:
+
+    string a: the first string
+    string b: the second string
+
+Returns
+
+    boolean: If a
+
+and b
+
+    are case-insensitive anagrams, return true. Otherwise, return false.
+
+Input Format
+
+The first line contains a string a
+.
+The second line contains a string b
+
+.
+
+Constraints
+
+Strings a and b
+
+    consist of English alphabetic characters.
+    The comparison should NOT be case sensitive.
+
+Sample Input 0
+
+anagram
+margana
+
+Sample Output 0
+
+Anagrams
+
+Explanation 0
+Character 	Frequency: anagram 	Frequency: margana
+A or a 	3 	3
+G or g 	1 	1
+N or n 	1 	1
+M or m 	1 	1
+R or r 	1 	1
+
+The two strings contain all the same letters in the same frequencies, so we print "Anagrams".
+
+Sample Input 1
+
+anagramm
+marganaa
+
+Sample Output 1
+
+Not Anagrams
+
+Explanation 1
+Character 	Frequency: anagramm 	Frequency: marganaa
+A or a 	3 	4
+G or g 	1 	1
+N or n 	1 	1
+M or m 	2 	1
+R or r 	1 	1
+
+The two strings don't contain the same number of a's and m's, so we print "Not Anagrams".
+
+Sample Input 2
+
+Hello
+hello
+
+Sample Output 2
+
+Anagrams
+
+Explanation 2
+Character 	Frequency: Hello 	Frequency: hello
+E or e 	1 	1
+H or h 	1 	1
+L or l 	2 	2
+O or o 	1 	1
+
+The two strings contain all the same letters in the same frequencies, so we print "Anagrams".
+*/
+import java.util.Scanner;
+
+public class Solution {
+    static boolean isAnagram(String a, String b) {
+        /* Complete the function
+         find the char at each pos and count it in string A 
+        do the same thing in String B and compare the each char with B
+       char first_alphabet_a=a.charAt(0);
+        char frist_alphabet_b=b.charAt(0);
+        if (a.length()!=b.length()){
+            System.out.println("Not Anagrams");
+        }
+        else{
+            for (int i=0;i<a.length()-1;i++){
+                char value_char
+            }
+        }
+        return true;
+        */
+        
+        char a_string[]=a.toLowerCase().toCharArray();
+        char b_string[]=b.toLowerCase().toCharArray();
+        boolean returnValue;
+        if(a_string !=null && b_string!=null){
+            java.util.Arrays.sort(a_string);
+            java.util.Arrays.sort(b_string);
+        returnValue=java.util.Arrays.equals(a_string,b_string);
+            return returnValue; 
+        }
+        else{
+            return returnValue=false;
+        }  
+        }
+    
+
+
+  public static void main(String[] args) {
+    
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+}
