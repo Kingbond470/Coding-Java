e78c3a7f777dd736f1082b0850b36c22ca127eeb
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-04-29 16:16:04 +0530

    Min Max Sum
    Using Loop and sort()

---
+++ Problem Solving/MinMaxSum.java
@@ -0,0 +1,131 @@
+/*
+[Min Max Sum](https://www.hackerrank.com/challenges/mini-max-sum/problem)
+Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+
+Example
+The minimum sum is and the maximum sum is
+
+. The function prints
+
+16 24
+
+Function Description
+
+Complete the miniMaxSum function in the editor below.
+
+miniMaxSum has the following parameter(s):
+
+    arr: an array of 
+
+    integers
+
+Print
+
+Print two space-separated integers on one line: the minimum sum and the maximum sum of
+of
+
+elements.
+
+Input Format
+
+A single line of five space-separated integers.
+
+Constraints
+
+Output Format
+
+Print two space-separated long integers denoting the respective minimum and maximum values that can be calculated by summing exactly four of the five integers. (The output can be greater than a 32 bit integer.)
+
+Sample Input
+
+1 2 3 4 5
+
+Sample Output
+
+10 14
+
+Explanation
+
+The numbers are
+, , , , and
+
+. Calculate the following sums using four of the five integers:
+
+    Sum everything except 
+
+, the sum is
+.
+Sum everything except
+, the sum is
+.
+Sum everything except
+, the sum is
+.
+Sum everything except
+, the sum is
+.
+Sum everything except
+, the sum is
+
+    .
+
+Hints: Beware of integer overflow! Use 64-bit Integer.
+
+Need help to get started? Try the Solve Me First problem
+*/
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<Integer> arr) {
+    // Write your code here
+    int arr_size=arr.size();
+    long max_sum=0;
+    long min_sum=0;
+    Collections.sort(arr);
+    int first=arr.get(0);
+    int last=arr.get(arr_size-1);
+    for (int i=0; i<arr_size;i++){
+        
+        max_sum=max_sum+arr.get(i);
+        min_sum=min_sum+arr.get(i);
+        
+    }
+    max_sum=max_sum-first;
+    min_sum=min_sum-last;
+    System.out.print(min_sum);
+    System.out.print(" ");
+    System.out.println(max_sum);
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        Result.miniMaxSum(arr);
+
+        bufferedReader.close();
+    }
+}
