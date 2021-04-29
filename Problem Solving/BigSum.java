d3a709d5d403a178b3f67ccf6fae22b7873cebae
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-04-29 11:19:52 +0530

    Big Sum
    Using Long data type

---
+++ Problem Solving/BigSum.java
@@ -0,0 +1,102 @@
+/*
+[Big Sum Problem](https://www.hackerrank.com/challenges/a-very-big-sum/problem)
+In this challenge, you are required to calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
+
+Function Description
+
+Complete the aVeryBigSum function in the editor below. It must return the sum of all array elements.
+
+aVeryBigSum has the following parameter(s):
+
+    int ar[n]: an array of integers .
+
+Return
+
+    long: the sum of all array elements
+
+Input Format
+
+The first line of the input consists of an integer
+.
+The next line contains
+
+space-separated integers contained in the array.
+
+Output Format
+
+Return the integer sum of the elements in the array.
+
+Constraints
+
+Sample Input
+
+5
+1000000001 1000000002 1000000003 1000000004 1000000005
+
+Output
+
+5000000015
+
+Note:
+
+The range of the 32-bit integer is
+
+.
+
+When we add several integer values, the resulting sum might exceed the above range. You might need to use long int C/C++/Java to store such sums. 
+*/
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'aVeryBigSum' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts LONG_INTEGER_ARRAY ar as parameter.
+     */
+
+    public static long aVeryBigSum(List<Long> ar) {
+    // Write your code here
+    //find the list size and use loop to find the sum using each iteration
+    int list_legnth=ar.size();
+    long sum=0;
+    for(int i=0; i<list_legnth;i++){
+        sum=sum+ar.get(i);
+        
+    }
+    return sum;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Long> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Long::parseLong)
+            .collect(toList());
+
+        long result = Result.aVeryBigSum(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
