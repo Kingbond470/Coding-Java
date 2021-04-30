1c3e24fa47df34bbc6e8c920505a8bcfac72e472
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-04-30 09:52:21 +0530

    Birthday Cake
    Using sort(), loop and if conditions

---
+++ Problem Solving/BirthdayCake.java
@@ -0,0 +1,62 @@
+/*
+https://www.hackerrank.com/challenges/birthday-cake-candles/problem
+*/
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'birthdayCakeCandles' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY candles as parameter.
+     */
+
+    public static int birthdayCakeCandles(List<Integer> candles) {
+    // Write your code here
+    // find the size of candles, sort the list and find the max element and compare it with each element and increase the count, if it found again and return it    
+    int length_candles=candles.size();
+    int count=0;
+    Collections.sort(candles);
+    int max_element=candles.get(length_candles-1);
+    for(int i=0; i<length_candles; i++ ){
+        if(max_element==candles.get(i)){
+            count=count+1;
+        }
+    }
+    return count;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int candlesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> candles = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.birthdayCakeCandles(candles);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
