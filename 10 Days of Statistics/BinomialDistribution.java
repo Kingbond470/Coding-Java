4d40db1019594e57648d5d1f26b5b29474705b6d
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-05-05 13:02:55 +0530

    Solving in process

---
+++ 10 Days of Statistics/BinomialDistribution.java
@@ -0,0 +1,22 @@
+// still solving
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        // B:G= 1.09:1 and n=6 and x>=3 p for boys 0.5 and for girl 0.5
+        double b=1.09;
+        double g=1;
+       double p_boys=b/(b+g);
+       double p_g=g/(b+g);
+       int x=3;
+       int n=6;
+        //double p_boys=0.522;
+      // double  p_g=0.478;
+       for(int r=x; r<=n; r++){
+           // use formula (x,n,p)
+       }
+    }
+}
