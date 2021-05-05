c8eb64348496250e680833b774c5e53e599628d1
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-05-05 18:47:41 +0530

    Day 4 | Geometric Distribution
    Using Hard-core input

---
+++ 10 Days of Statistics/GeometriDistribution.java
@@ -0,0 +1,21 @@
+/*
+https://www.hackerrank.com/challenges/s10-geometric-distribution-1/problem
+*/
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        // the product of defect is 1/3 so p =1/3 and q=1-p and use the formula g(n,p)=q^(n-1)*p
+        
+        double p=0.33333333333  ;    // 1/3
+        //System.out.println(p);
+        double q=1-p;
+        int n=5;
+        double result=0;
+        result=Math.pow(q,n-1) * p;
+        System.out.printf("%.3f",result);
+    }
+}
