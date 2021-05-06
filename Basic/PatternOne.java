7bac3a01b0e54ef4652e619c268c039ff838110e
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-05-06 20:09:44 +0530

    Using String
    #WithoutUsingTwoLoop

---
+++ Basic/PatternOne.java
@@ -0,0 +1,19 @@
+//WithoutUsingTwoLoop
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("Hello World!");
+        String value="";
+        for(int i=1;i<=5;i++){
+                    value=value+i+" ";
+                    System.out.println(value);
+        }
+    }
+}
+
+// output
+// Hello World!
+// 1 
+// 1 2 
+// 1 2 3 
+// 1 2 3 4 
+// 1 2 3 4 5 
