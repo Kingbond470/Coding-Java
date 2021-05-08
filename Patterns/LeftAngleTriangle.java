710a7cc83cc271d10675c65000042a53b54b3a72
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-05-08 16:33:21 +0530

    Left Angle Triangle | Pattern 3
    Using two loop and if-else

---
+++ Patterns/LeftAngleTriangle.java
@@ -0,0 +1,28 @@
+//Left angle triangle star pattern
+import java.util.Scanner;
+public class Main{
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(i+j<n){
+                    System.out.print("  ");
+                }
+                else{
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+// output- 
+//  n=6
+            
+//           * 
+//         * * 
+//       * * * 
+//     * * * * 
+//   * * * * * 
