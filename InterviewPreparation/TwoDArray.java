9d7013cf2b699ba9eedea762cc5d8a3e9cb7d43e
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-05-11 19:18:59 +0530

    2D Array

---
+++ InterviewPreparation/TwoDArray.java
@@ -0,0 +1,76 @@
+/*
+https://www.hackerrank.com/challenges/2d-array/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
+*/
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'hourglassSum' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int hourglassSum(List<List<Integer>> arr) {
+    // Write your code here
+    // we can access the element using arr.get(i).get(j) then add the particular value upto length-2 and compare it with max sum
+    //System.out.println(arr.get(0).get(0));
+    
+    int sum=0;
+    int max_sum=-10000;         //so that sum of negative is also included
+    for(int i=0; i<arr.size()-2; i++){      // row value -i
+        for(int j=0; j<arr.size()-2; j++){      // column value- j
+            sum=arr.get(i).get(j)+arr.get(i).get(j+1)+arr.get(i).get(j+2)+arr.get(i+1).get(j+1)+arr.get(i+2).get(j)+arr.get(i+2).get(j+1)+arr.get(i+2).get(j+2);
+          //System.out.print(sum+ " ");         // each pattern sum
+           if(sum>max_sum){
+            max_sum=sum;  
+        }
+        }
+       
+    }
+   // System.out.print(max_sum);
+   // int max_sum=0;
+    return max_sum;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        IntStream.range(0, 6).forEach(i -> {
+            try {
+                arr.add(
+                    Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        int result = Result.hourglassSum(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
