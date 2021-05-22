444203f8930f80476815dda1e36e0511ee3f3d56
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-05-22 19:58:35 +0530

    Java Sub Array | Using 3 Loop
    Negative sum

--- JavaSubArray.java
+++ JavaSubArray.java
@@ -33,3 +33,41 @@ public class Solution {
         
     }
 }
+
+// Another Approach
+/*
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        int[] arr=new int[size];
+        for(int i=0; i<size; i++){
+            arr[i]=sc.nextInt();
+        }
+        int count=0;
+        //make a sub array and add them and check if it is <0 then increase count
+        for(int i=0; i<size; i++){
+           
+            for(int j=i;  j<size; j++){
+                 int sum=0;
+                for(int k=i; k<=j; k++){
+                   // System.out.print(arr[k]+" ");
+                    // to print all elements of sub array
+                    sum=sum+arr[k];
+                }
+                if(sum<0){
+                    count++;
+                }
+            }
+            //System.out.println();
+        }
+        System.out.println(count);
+        
+    }
+}
+*/
