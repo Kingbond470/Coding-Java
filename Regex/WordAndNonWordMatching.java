cbd8638c6d4b3252cbec39a9c5286c1609258aa0
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-05-26 17:54:04 +0530

    Word and Non-Word Problem | Regex
    Using \\w and \\W

---
+++ Regex/WordAndNonWordMatching.java
@@ -0,0 +1,31 @@
+/*
+https://www.hackerrank.com/challenges/matching-word-non-word/problem
+*/
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("\\w\\w\\w\\W\\w\\w\\w\\w\\w\\w\\w\\w\\w\\w\\W\\w\\w\\w"); // Use \\ instead of using \ 
+    
+    }
+}
+
+class Regex_Test {
+
+    public void checker(String Regex_Pattern){
+    
+        Scanner Input = new Scanner(System.in);
+        String Test_String = Input.nextLine();
+        Pattern p = Pattern.compile(Regex_Pattern);
+        Matcher m = p.matcher(Test_String);
+        System.out.println(m.find());
+    }   
+    
+}
