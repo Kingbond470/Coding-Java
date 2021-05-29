0a28390c3fabb3dee6c43028fe523339d6cbcf3b
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-05-29 09:44:33 +0530

    Matching a specific character
    Using [ ]

---
+++ Regex/MatchingSpecificCharacter.java
@@ -0,0 +1,31 @@
+/*
+https://www.hackerrank.com/challenges/matching-specific-characters/problem
+*/
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("^[123][120][xs0][30Aa][xsu][.,]$");// [] it matches only one character out of them 
+    
+    }
+}
+
+class Regex_Test {
+
+    public void checker(String Regex_Pattern){
+    
+        Scanner Input = new Scanner(System.in);
+        String Test_String = Input.nextLine();
+        Pattern p = Pattern.compile(Regex_Pattern);
+        Matcher m = p.matcher(Test_String);
+        System.out.println(m.find());
+    }   
+    
+}
