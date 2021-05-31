2488f6405a37e8196cd453eb8f2389205eab2667
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-05-31 14:50:10 +0530

    Target Array
    Using array and arraylist add() method

---
+++ Leetcode/Array/CreateTargetArray.java
@@ -0,0 +1,103 @@
+/*
+Given two arrays of integers nums and index. Your task is to create target array under the following rules:
+    Initially target array is empty.
+    From left to right read nums[i] and index[i], insert at index index[i] the value nums[i] in target array.
+    Repeat the previous step until there are no elements to read in nums and index.
+Return the target array.
+It is guaranteed that the insertion operations will be valid.
+ 
+Example 1:
+Input: nums = [0,1,2,3,4], index = [0,1,2,2,1]
+Output: [0,4,1,3,2]
+Explanation:
+nums       index     target
+0            0        [0]
+1            1        [0,1]
+2            2        [0,1,2]
+3            2        [0,1,3,2]
+4            1        [0,4,1,3,2]
+Example 2:
+Input: nums = [1,2,3,4,0], index = [0,1,2,3,0]
+Output: [0,1,2,3,4]
+Explanation:
+nums       index     target
+1            0        [1]
+2            1        [1,2]
+3            2        [1,2,3]
+4            3        [1,2,3,4]
+0            0        [0,1,2,3,4]
+Example 3:
+Input: nums = [1], index = [0]
+Output: [1]
+ 
+Constraints:
+    1 <= nums.length, index.length <= 100
+    nums.length == index.length
+    0 <= nums[i] <= 100
+    0 <= index[i] <= iGiven two arrays of integers nums and index. Your task is to create target array under the following rules:
+    Initially target array is empty.
+    From left to right read nums[i] and index[i], insert at index index[i] the value nums[i] in target array.
+    Repeat the previous step until there are no elements to read in nums and index.
+Return the target array.
+It is guaranteed that the insertion operations will be valid.
+ 
+Example 1:
+Input: nums = [0,1,2,3,4], index = [0,1,2,2,1]
+Output: [0,4,1,3,2]
+Explanation:
+nums       index     target
+0            0        [0]
+1            1        [0,1]
+2            2        [0,1,2]
+3            2        [0,1,3,2]
+4            1        [0,4,1,3,2]
+Example 2:
+Input: nums = [1,2,3,4,0], index = [0,1,2,3,0]
+Output: [0,1,2,3,4]
+Explanation:
+nums       index     target
+1            0        [1]
+2            1        [1,2]
+3            2        [1,2,3]
+4            3        [1,2,3,4]
+0            0        [0,1,2,3,4]
+Example 3:
+Input: nums = [1], index = [0]
+Output: [1]
+ 
+Constraints:
+    1 <= nums.length, index.length <= 100
+    nums.length == index.length
+    0 <= nums[i] <= 100
+    0 <= index[i] <= i
+*/
+
+import java.util.ArrayList;
+import java.util.Scanner;
+public class NumsAndIndexArray {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the array num and index size : ");
+        int n=sc.nextInt();
+        int[] num=new int[n];
+        int[] index=new int[n];
+
+        for(int i=0; i<n; i++){
+            num[i]=sc.nextInt();
+        }
+
+        for(int i=0; i<n; i++){
+            index[i]=sc.nextInt();
+        }
+        ArrayList<Integer> new_arr=new ArrayList<>();
+        for(int i=0; i<n; i++){
+            int index_value=index[i];
+            int num_value=num[i];
+            new_arr.add(index_value,num_value);
+        }
+for(int a: new_arr){
+    System.out.print(a+" ");
+}
+    }
+}
+
