f5f72904ab2f37d86b65d0254087165c8fe283e7
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-06-01 21:17:22 +0530

    PI Relation Value | Random Value 0-1
    Using Math.random()

--- UniqueProblem/RelationOfPI.java
+++ UniqueProblem/RelationOfPI.java
@@ -1,23 +1,27 @@
+import java.util.Scanner;
 public class Main {
-    public static double FindPi(n){
-        int total_square_points=0;
-        int total_circle_points=0;  
+    public static void FindPi(int n){
+        double total_square_points=0;
+        double total_circle_points=0;  
+        double distance;
         for(int i=0; i<n; i++){
             double x=Math.random();
             double y=Math.random();
-            distance =Math.pow(x,2)+Math.pow(y,2);
-            if distance<=1{
+            //double distance =Math.pow(x,2)+Math.pow(y,2);
+            distance=x*x+y*y;
+            if (distance<1){
                 total_circle_points++;
             }
             total_square_points++;
         }
-        double pi=4*total_circle_points/total_square_points;
-        return pi;
+        double pi=4*(total_circle_points/total_square_points);
+        System.out.println(pi);
+        //return pi;
     }
     public static void main(String args[]) {
-      //  System.out.println("Hello World!");
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-       System.out.println(FindPi(n));
+        FindPi(n);
+      //  System.out.println(FindPi(n));
     }
 }
