0502abf724b8c9c67cc2c95702b20d8b01509ca4
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-06-05 19:29:02 +0530

    Find The Highest Altitude | Another Approach
    Using for and if-else

--- Leetcode/Array/FindHighestAltitude.java
+++ Leetcode/Array/FindHighestAltitude.java
@@ -23,3 +23,22 @@ class Solution {
         return max;
     }
 }
+/*
+class Solution {
+    public int largestAltitude(int[] gain) {
+      int altitude=0;
+        int max=-999;
+        for(int i=0; i <gain.length; i++){
+            altitude=altitude+gain[i];
+            if(altitude>max){
+                max=altitude;
+            }
+        }
+        if(max<0){
+            max=0;
+        }
+        return max;
+  
+    }
+}
+*/
