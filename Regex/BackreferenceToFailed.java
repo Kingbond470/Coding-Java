c3bf2249ac2427d445f1d7dc6cce8b8f4a235441
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-06-14 20:43:40 +0530

    Backreference To Failed | Regex
    Using {}

---
+++ Regex/BackreferenceToFailed.java
@@ -0,0 +1,31 @@
+/*
+https://www.hackerrank.com/challenges/backreferences-to-failed-groups/problem
+*/
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("^(\\d{8})$|^((\\d\\d)-){3}(\\d\\d)$"); 
+    
+    }
+}
+
+class Regex_Test {
+
+    public void checker(String Regex_Pattern){
+    
+        Scanner Input = new Scanner(System.in);
+        String Test_String = Input.nextLine();
+        Pattern p = Pattern.compile(Regex_Pattern);
+        Matcher m = p.matcher(Test_String);
+        System.out.println(m.find());
+    }   
+    
+}
