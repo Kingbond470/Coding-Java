654cc84db1fdd2c64bfd88a94adddd8b9612e4b7
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-06-16 11:23:28 +0530

    Update MinimumArrayUpdate.java

--- OnlineJudge/Array/MinimumArrayUpdate.java
+++ OnlineJudge/Array/MinimumArrayUpdate.java
@@ -16,3 +16,34 @@ public class Main{
     System.out.println(k>arr[0]?k-arr[0]:0);
   }
 }
+
+//Another approach using count
+import java.util.Arrays;
+import java.util.Scanner;
+public class Main{
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int k=sc.nextInt();
+    int[] arr=new int[n];
+    for(int i=0; i<n; i++){
+      arr[i]=sc.nextInt();
+    }
+    
+    int max_count=0;
+    for(int i=0; i<n; i++){
+      int count=0;
+      while(arr[i]<k){
+        arr[i]++;
+       // System.out.print(arr[i]+" ");
+        count++;
+      }
+      if(count>max_count) max_count=count;
+
+    }
+     
+   System.out.println(max_count);
+        
+
+  }
+}
