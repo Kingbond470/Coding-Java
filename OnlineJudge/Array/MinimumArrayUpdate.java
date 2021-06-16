a29bc61d0540dd38ad17bd7c157fc8a3e9c712b3
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-06-16 11:28:39 +0530

    Minimum Array Update Required | OnlineJudge
    Using count, k-minimum value of array

--- OnlineJudge/Array/MinimumArrayUpdate.java
+++ OnlineJudge/Array/MinimumArrayUpdate.java
@@ -47,3 +47,26 @@ public class Main{
 
   }
 }
+
+//using k-min value of array
+import java.util.Arrays;
+import java.util.Scanner;
+public class Main{
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int k=sc.nextInt();
+    int[] arr=new int[n];
+    for(int i=0; i<n; i++){
+      arr[i]=sc.nextInt();
+    }
+    int min=arr[0];
+    for(int i=0; i<n; i++){
+		if(arr[i]<min) min=arr[i];
+    }
+    int min_step=0;
+    if(min<k) min_step=k-min;
+    System.out.print(min_step);
+
+  }
+}
