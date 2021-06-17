de40efce439c9ecf4f69220d9a8ce05e07fe823a
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-06-17 18:05:57 +0530

    Matrix Diagonal Sum | Leetcode

--- Leetcode/Array/MatrixDiagonalSum.java
+++ Leetcode/Array/MatrixDiagonalSum.java
@@ -17,3 +17,19 @@ class Solution {
         return sum;
     }
 }
+
+
+//another method
+class Solution {
+    public int diagonalSum(int[][] mat) {
+     //primary diagonal is i==j
+    // secondary diagonla is i+j
+        int sum=0;
+       for(int i=0; i<mat.length; i++){
+           sum+=mat[i][i];
+           sum+=mat[mat.length-i-1][i];
+       }
+       if (mat.length%2==1) sum=sum-mat[mat.length/2][mat.length/2] ;
+        return sum;
+    }
+}
