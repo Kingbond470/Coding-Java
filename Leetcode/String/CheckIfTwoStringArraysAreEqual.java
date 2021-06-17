49f77aa733f8f46bb354b4a9db25a7ef26444eca
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-06-17 18:27:06 +0530

    Two Array String Equal | Leetcode

--- Leetcode/String/CheckIfTwoStringArraysAreEqual.java
+++ Leetcode/String/CheckIfTwoStringArraysAreEqual.java
@@ -12,3 +12,51 @@ class Solution {
         return word1_str.equals(word2_str);
     }
 }
+
+
+//using function
+class Solution {
+    public static String getString(String[] word){
+        String word_str="";
+        for(int i=0; i<word.length; i++){
+            word_str=word_str+word[i];
+        }
+        return word_str;
+    }
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {        
+        return getString(word1).equals(getString(word2));
+    }
+    
+}
+
+//0ms using stringBuilder
+class Solution {
+    
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {        
+        StringBuilder sb1=new StringBuilder();
+        StringBuilder sb2=new StringBuilder();
+        
+        for(String word: word1) sb1.append(word);
+        for(String word: word2) sb2.append(word);
+        
+        String s1=sb1.toString();
+        String s2=sb2.toString();
+        return s1.equals(s2);
+    }
+    
+}
+
+
+//using join
+class Solution {
+    
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {  
+        String s2="", s3="";
+        String s1=s2.join("", word1);
+        String s4=s3.join("", word2);
+        return s1.equals(s4);
+        // return s1.join("",word1).equals(s2.join("",word2));
+        
+    }
+    
+}
