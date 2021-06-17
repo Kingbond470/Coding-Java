89af55f145703892ac4448f7cf18910dfd5a6450
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-06-17 19:51:07 +0530

    Minimum Operation Make Array Increasing|Leetcode

--- Leetcode/Array/MinimuOperationToMakeArrayIncreasing.java
+++ Leetcode/Array/MinimuOperationToMakeArrayIncreasing.java
@@ -12,3 +12,51 @@ class Solution {
         return count;
     }
 }
+
+
+//another
+class Solution {
+    public int minOperations(int[] nums) {
+        if(nums.length==0) return 0;
+        
+        int count=0;
+        int num=nums[0];
+        for(int i=1; i<nums.length; i++){
+            if(num==nums[i]){
+                num++;
+                count++;
+            }
+            else if(num>nums[i]){
+                num++;
+                count=count+num-nums[i];
+            }
+            else{
+                num=nums[i];
+            }
+        }
+        return count;
+    }
+}
+
+//easy logic
+
+//     Start from index 1
+//     if the current element (nums[i]) is less than the previous element(nums[i-1]) then have the difference count to store the difference, also +1 to make it increasing
+//     Add the difference to the result count += diff
+//     Add the difference to the current element nums[i] += diff
+//     proceed till the end (nums.length)
+//     return count
+
+class Solution {
+    public int minOperations(int[] nums) {
+        int count = 0;
+        for (int i = 1;i<nums.length;i++){
+            if (nums[i] <= nums[i-1]) {
+                int diff = nums[i-1] - nums[i] + 1;
+                count += diff;
+                nums[i] += diff;
+            }
+        }
+        return count;
+    }
+}
