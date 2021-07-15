7c54534736a26b0e2bedbfd02bd6ab2724e423f3
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-07-15 17:16:45 +0530

    Update PushPopTop.java

--- OnlineJudge/Stack/PushPopTop.java
+++ OnlineJudge/Stack/PushPopTop.java
@@ -21,3 +21,39 @@ public class Main{
     }
   }
 }
+
+
+// Another approach
+/*
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        Stack<Integer> stack = new Stack<Integer>();
+        int top = -1;
+        int[] arr = new int[10];
+        for(int a=0; a<t; a++){
+            int n = sc.nextInt();
+            if(n==1){
+                top++;
+                arr[top] = sc.nextInt();
+            }
+            if(n==2){
+                if(top>=0){
+                    top--;
+                }
+            }
+            if(n==3){
+                if(top != -1) System.out.println(arr[top]);
+                else System.out.println("Empty!");
+                
+            }
+        }
+    }
+}
+
+        
+*/
