f9a81b4d4aa0386da9690844aa11a62da8304f71
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-10-08 12:08:57 +0530

    Update MajorityElementArray.java

--- Revision/MajorityElementArray.java
+++ Revision/MajorityElementArray.java
@@ -1,20 +1,73 @@
+// https://oj.masaischool.com/contest/2088/problem/1-1
+
 // Brute Force
-public class Main {
-    public static void main(String args[]) {
-        int[] arr={3, 3, 4, 2, 4, 3,2, 4, 4};
-        int max=0;
-        for(int i=0; i<arr.length; i++){
-            int count=0; 
-            for(int j=0 ; j<arr.length; j++){
-                if(arr[i]==arr[j]) count++;
+//Enter code here
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int a=0; a<t; a++){
+            int n=sc.nextInt();
+            int[] arr=new int[n];
+            for(int i=0; i<n; i++) arr[i]=sc.nextInt();
+            
+            int max=0;
+            int idxValue=-1;
+            for(int i=0; i<n; i++){
+                int count=0;
+                for(int j=0; j<n; j++){
+                    if(arr[i]==arr[j]) count++;
+                }
+                if(count>n/2){
+                    max=count;
+                    idxValue=arr[i];
+                    break;
+                }
             }
-            if(count>max) max=count;
+            System.out.println(idxValue);
         }
-        System.out.println(max>arr.length/2?"yes":"no");
+    }
+}
+// tc- O(n^2)
+// sc- O(1)
+
+// HashMap Appraoch
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int a=0; a<t; a++){
+            int n=sc.nextInt();
+            int[] arr=new int[n];
+            for(int i=0; i<n; i++)arr[i]=sc.nextInt();
+            
+            HashMap<Integer, Integer> map=new HashMap<>();
+            for(int i=0; i<n; i++){
+                if(map.containsKey(arr[i])){
+                    int value=map.get(arr[i]);
+                    map.put(arr[i], ++value);
+                }
+                else {
+                    map.put(arr[i], 1);
+                }
+            }
+            
+            int val=-1;
+            for(Map.Entry<Integer,Integer> set:map.entrySet()){
+               // System.out.println(set.getValue());
+                if(set.getValue()>n/2){
+                    val=set.getKey();
+                } 
+            }
+            
+            
+            System.out.println(val);
+        }
     }
 }
 /*
-Input - {3, 3, 4, 3, 4, 4, 2, 4, 4}
-Output- Yes
-Input - {3, 2, 3, 3, 4, 4, 2, 4, 4}
-Input - No
+Tc- O(nlogn)
+Sc- O(n)
+*/
