dc9d090ddcfbb25cd89d0ad9575ac71eee25f23e
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-10-13 16:32:04 +0530

    Create PrintMasaiSchoo.java

---
+++ OnlineJudge/7DaysOfMasai/PrintMasaiSchoo.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Masai School");
+        System.out.print("A Transformation in Education");
+        
+    }
+}
